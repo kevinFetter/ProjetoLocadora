705ff5509653a1b50ad2cac07c4fc23b0f2eee55
Thiago Martins Saraiva <thiagomartinssaraiva@gmail.com>
2019-10-07 23:30:57 -0300

    inicio do desenvolvimento da main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+  printf("Menu de opcoes.\n");
+  printf("1 - Cadastrar um novo filme.\n");
+  printf("2 - Editar um filme existente.\n");
+  printf("3 - Procurar por um filme por meio de titulo ou codigo.\n");
+  printf("4 - Gerar relatorio.\n");
+  printf("5 - Consultar informacoes do filme.\n");
+  printf("6 - Gerar relatorio em formato PDF.\n");
+}
