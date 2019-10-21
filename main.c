79aff799b1ac7ad5ecbd85fde0f63d902832058a
Thiago <thiagomartinssaraiva@gmail.com>
2019-10-21 20:24:45 -0300

    teste de leitura e escrita de arquivos

--- main.c
+++ main.c
@@ -1,8 +1,62 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 void main() {
 
   int opcao;
+
+  // struct file {
+  //   int a, b;
+  // };
+
+  int fd;
+  
+  // struct file t;
+
+  printf("fd = %d\n", fd);
+
+  if (fd == -1) {
+    printf("Error\n");
+  }
+
+  printf("opened the fd = %d\n", fd);
+
+  int close(int fd);
+
+  if(close(fd) < 0) {
+    perror("c1");
+  }
+
+  printf("closed the fd\n");
+
+  size_t read (int fd, void* buf, size_t cnt);
+
+  // int  sz;
+
+  // char *c = (char *) calloc(100,sizeof(char));
+
+  // sz = read(fd, c, 10);
+  
+  // printf("called read(%d, c, 10).returned that"
+  //       " %d bytes were read. \n",fd, sz);
+  // c[sz] = '\0';
+
+  // printf("Those bytes are as follow: %s\n", c);
+
+  int sz; 
+  char *c = (char *) calloc(100, sizeof(char)); 
+  
+  fd = open("testfile.txt", O_RDONLY); 
+  if (fd < 0) { perror("r1"); exit(1); } 
+  
+  sz = read(fd, c, 10); 
+  printf("called read(% d, c, 10).  returned that"
+        " %d bytes  were read.\n", fd, sz); 
+  c[sz] = '\0'; 
+  printf("Those bytes are as follows: %s\n", c); 
 
   struct Filme {
     char titulo[60];
